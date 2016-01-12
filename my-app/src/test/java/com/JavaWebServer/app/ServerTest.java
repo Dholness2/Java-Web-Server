bb01dbeb28e0193b9f82ab7dc5f1a4cd2a2153cf
Donovan Holness <dholness@8thlight.com>
2016-01-12 15:25:00 -0800

    created server test for simple GET request

--- my-app/src/test/java/com/JavaWebServer/app/ServerTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/ServerTest.java
@@ -1,14 +1,27 @@
 package com.JavaWebServer.app;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import org.junit.Before;
+
+import java.net.*;
+import java.io.*;
 
 public class ServerTest  {
-
+  @Before 
+  public void setupServer () {
+    int  port = 9090;
+    Server TestServer = new Server(port);
+    TestServer.run; 
+   }
+ 
   @Test 
-  public void creatsSeverObject () {
-      Server testServer = new Server();
-      int port  = testServer.getPort();
-      assertEquals(5000, port);
-  }
+  public void ServerGetsCleintMessage () {
+   Socket clientTestSocket = new Socket("Local Host", 9090);
+   PrintWriter outputs = new PrintWriter(clientTestSocket.getOutputStream(), true);
+   BufferedReader inputs =new BufferedReader( new InputStreamReader(clientTestSocket.getInputStream()));
+   outputs.print("GET / HTTP/1.1");  
+ }
 }

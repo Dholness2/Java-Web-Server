dc72521cbbd74a6e775128a197adc6a848df3cfc
dholness2 <DHolness@Dons-MacBook-Pro.local>
2016-01-17 15:00:50 -0800

    Updated ServerGetsCleintMessage test  -changed print to println  -changed response assertion

--- my-app/src/test/java/com/JavaWebServer/app/ServerTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/ServerTest.java
@@ -14,7 +14,7 @@ public class ServerTest  {
   
   @Before 
   public void setupServer () {
-    int  port = 9999;
+    int  port = 8787;
     testServer = new Server(port);
   }
 
@@ -22,18 +22,17 @@ public class ServerTest  {
   public void ServerGetsCleintMessage () throws Exception {
     Thread testThread =  new Thread(testServer);
     testThread.start();
-    testThread.sleep(100);
+    testThread.sleep(100); 
     System.out.println("pre connection");  
  
-    Socket clientTestSocket = new Socket("localhost", 9999);
+    Socket clientTestSocket = new Socket("localhost", 8787);
     System.out.println( "Client connected to Server");
     PrintWriter outPut = new PrintWriter( clientTestSocket.getOutputStream(), true);
-    outPut.print("GET / HTTP/1.1");
+    outPut.println("GET / HTTP/1.1");
     System.out.println("message sent from Client");
     InputStreamReader stream = new InputStreamReader(clientTestSocket.getInputStream());
     BufferedReader response = new BufferedReader (stream);
     String message  = response.readLine();
-    clientTestSocket.close();
-    assertEquals("200",message);
+    assertEquals("HTTP/1.1 200 ok",message);
     }
   }

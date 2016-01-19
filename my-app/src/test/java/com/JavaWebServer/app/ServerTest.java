95f07f40ebb94eaf29cf7ef154925c0cb8f4c507
Donovan Holness <dholness@8thlight.com>
2016-01-18 17:58:49 -0800

    updated tests
     * added private readresponse
     * added hellworld assertion

--- my-app/src/test/java/com/JavaWebServer/app/ServerTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/ServerTest.java
@@ -1,7 +1,7 @@
 package com.JavaWebServer.app;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -18,21 +18,47 @@ public class ServerTest  {
     testServer = new Server(port);
   }
 
+  @Test
+  public void TestisServerOnEqualsTrue () {
+    assertTrue(testServer.isServerOn());
+    }
+  @Test
+  public void TestisServerOnEqualsFalse () {
+    testServer.off();
+    assertFalse(testServer.isServerOn());
+  }
+
+  @Test
+  public void TestServerTurnsoff () {
+    testServer.off();
+    assertFalse(testServer.isServerOn());
+  }
+
  @Test 
-  public void ServerGetsCleintMessage () throws Exception {
-    Thread testThread =  new Thread(testServer);
+  public void  TestServerGetsClientMessage () throws Exception {
+    Thread testThread = new Thread(testServer);
     testThread.start();
     testThread.sleep(100); 
-    System.out.println("pre connection");  
+    
  
     Socket clientTestSocket = new Socket("localhost", 8787);
-    System.out.println( "Client connected to Server");
+    System.out.println( "Client: connected to Server");
     PrintWriter outPut = new PrintWriter( clientTestSocket.getOutputStream(), true);
     outPut.println("GET / HTTP/1.1");
-    System.out.println("message sent from Client");
+    System.out.println("Client: message sent from Client Get /HTTP/1.1");
     InputStreamReader stream = new InputStreamReader(clientTestSocket.getInputStream());
-    BufferedReader response = new BufferedReader (stream);
-    String message  = response.readLine();
-    assertEquals("HTTP/1.1 200 ok",message);
-    }
+    BufferedReader response = new BufferedReader(stream);
+    String message =  readResponse(response);
+    assertEquals("HTTP/1.1 200 okHello world", message);
+    System.out.println("Client: Message Recieved " + message);
   }
+
+  private String readResponse (BufferedReader buffer) throws Exception {
+    String message ="";
+    String line;
+    while ((line = buffer.readLine()) != null) {
+      message += line;
+    }
+    return message;
+  }
+}

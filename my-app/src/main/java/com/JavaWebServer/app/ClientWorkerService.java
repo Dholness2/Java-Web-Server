f44e65181ea601074581fc857e0a5005e9f8fb22
Donovan Holness <dholness@8thlight.com>
2016-01-18 17:54:12 -0800

    removed spaces

--- my-app/src/main/java/com/JavaWebServer/app/ClientWorkerService.java
+++ my-app/src/main/java/com/JavaWebServer/app/ClientWorkerService.java
@@ -4,30 +4,30 @@ import java.net.*;
 import java.util.*;
 
 public class ClientWorkerService implements Runnable {
-  protected Socket clientSocket = null;
-  protected String serverName = null;
+  private Socket clientSocket = null;
+  private String serverName = null;
   
   public ClientWorkerService (Socket clientSocket, String serverName) {
-	  this.clientSocket = clientSocket;
-	  this.serverName = serverName;
+    this.clientSocket = clientSocket;
+    this.serverName = serverName;
   }
 
-  public void run() {
-   try {
-       InputStream input = clientSocket.getInputStream();
-       OutputStream output = clientSocket.getOutputStream();
-       System.out.println("Server: reading inputstream");
-       BufferedReader message = new BufferedReader(new InputStreamReader(input));
-       System.out.println(serverName +" Recevied message");
-       PrintWriter response = new PrintWriter (output,true);
-       response.println("HTTP/1.1 200 ok"+"\n");
-       response.println("Hello world");
-       output.close();
-       input.close();
-       clientSocket.close();
-       System.out.println ("request Closed");	    
+  public void run () {
+    try {
+          InputStream input = clientSocket.getInputStream();
+          OutputStream output = clientSocket.getOutputStream();
+          System.out.println("Server: reading inputstream");
+          BufferedReader message = new BufferedReader(new InputStreamReader(input));
+          System.out.println(serverName +" Recevied message");
+          PrintWriter response = new PrintWriter (output,true);
+          response.println("HTTP/1.1 200 ok"+"\n");
+          response.println("Hello world");
+          output.close();
+          input.close();
+          clientSocket.close();
+          System.out.println ("Server: request Closed");
    } catch (IOException e) {
-	   e.printStackTrace();
+     e.printStackTrace();
    }
   }
 }

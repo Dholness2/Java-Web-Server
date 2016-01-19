cc8ae321d6ff76ed1f9ae9cbc45547cf7f636304
Donovan Holness <dholness@8thlight.com>
2016-01-18 16:11:51 -0800

    removed extra spaces

--- my-app/src/main/java/com/JavaWebServer/app/Server.java
+++ my-app/src/main/java/com/JavaWebServer/app/Server.java
@@ -6,61 +6,53 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server implements Runnable{
- private String serverName = "localhost";
- private int port;
- private boolean serverOn = true;
- private ServerSocket serverSocket = null;
- private Thread runningThread = null;
- protected ExecutorService threadPool = Executors.newFixedThreadPool(10);
+  private String serverName = "localhost";
+  private int port;
+  private boolean serverOn = true;
+  private ServerSocket serverSocket = null;
+  private Thread runningThread = null;
+  protected ExecutorService threadPool = Executors.newFixedThreadPool(10);
  
- public Server (int port) {
+  public Server (int port) {
    this.port = port;
- }
+  }
 
- public void  run () {
-   synchronized(this){
+  public void run () {
+    synchronized(this){
     this.runningThread = Thread.currentThread();
-   }
-   openServerSocket();
-   System.out.println( "opened Server Socket");
-   while (isServerOn()) {
-     System.out.println("inside while");
-     Socket clientSocket  = null;
-     try {
-       clientSocket =  this.serverSocket.accept();
-       System.out.println("listening on port");
-     } catch (IOException e) {
+    }
+    openServerSocket();
+    System.out.println( "Server: opened Server Socket");
+    while (isServerOn()) {
+      Socket clientSocket  = null;
+      try {
+        clientSocket =  this.serverSocket.accept();
+        System.out.println("Server: listening on port");
+      } catch (IOException e) {
         if(serverOn == false) {
           System.out.println("Server off "+ e);
-          return;
-     } 
-     throw new RuntimeException(
-          "Error accepting client connection", e);
-     } 
-     this.threadPool.execute(new ClientWorkerService(clientSocket,serverName));
-    }
-   this.threadPool.shutdown();
-   System.out.println("Server off");
- }
- 
- private void openServerSocket() {
-  try { 
-      this.serverSocket = new ServerSocket(port);
-      } catch(IOException e) {
-        throw new RuntimeException("cannont open port"+ port ,e);
+          break;
+        } throw new RuntimeException( "Error accepting client connection", e);
       }
- }
-
- private synchronized boolean isServerOn() {
-  return this.serverOn;
- }
+      this.threadPool.execute(new ClientWorkerService(clientSocket,serverName));
+    }
+    this.threadPool.shutdown();
+    System.out.println("Server off");
+  }
  
- public synchronized void off() {
-   this.serverOn = false;
-   try {
-     this.serverSocket.close();
-   } catch (IOException e) {
-   throw new RuntimeException("Error closing server", e);
-   }
- }
+  private void openServerSocket() {
+    try { 
+      this.serverSocket = new ServerSocket(port);
+       } catch(IOException e) {
+         throw new RuntimeException("cannont open port"+ port ,e);
+       }
+  }
+
+  public synchronized boolean isServerOn() {
+    return this.serverOn;
+  }
+ 
+  public synchronized void off() {
+    this.serverOn = false;
+  }
 }

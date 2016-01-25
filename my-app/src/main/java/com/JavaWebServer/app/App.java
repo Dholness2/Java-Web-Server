d29d1cea71df9c58f5ed20cfbfe9e048072497a6
Donovan Holness <dholness@8thlight.com>
2016-01-25 15:29:16 -0800

    added wrapper sockets to main

--- my-app/src/main/java/com/JavaWebServer/app/App.java
+++ my-app/src/main/java/com/JavaWebServer/app/App.java
@@ -1,8 +1,12 @@
 package com.JavaWebServer.app;
+import java.net.*;
 
 public class App {
-  public static void main( String[] args) {
-    Server app  = new Server(5000);
+  public static void main( String[] args) throws Exception {
+    int port = 5000;
+    ServerSocket socket = new ServerSocket(port);
+    ServerSocketWrapper serverSocket = new ServerSocketWrapper(socket);
+    Server app = new Server(port,serverSocket);
     new Thread(app).start();
   }
 }

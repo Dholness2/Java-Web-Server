c68ad40d9724ac2b9aff446935612854ed669b5d
Donovan Holness <dholness@8thlight.com>
2016-02-03 13:14:22 -0800

    implemented options parser

--- my-app/src/main/java/com/JavaWebServer/app/App.java
+++ my-app/src/main/java/com/JavaWebServer/app/App.java
@@ -1,9 +1,15 @@
 package com.JavaWebServer.app;
+
+import java.util.Map;
 import java.util.HashMap;
 
 public class App {
+  private static final int PORT_INDEX = 0;  
+  private static final String [] KEYS = {"-p","-d"};
+  
   public static void main( String[] args) throws Exception {
-    int port = 5000;
+    Map<String,String> parsedArguments = OptionsParser.parse(args, KEYS);
+    int port = Integer.parseInt(parsedArguments.get(KEYS[PORT_INDEX]));
     ServerSocket serverSocket = new ServerSocket(port);
     Server app = new Server(port,serverSocket);
     new Thread(app).start();

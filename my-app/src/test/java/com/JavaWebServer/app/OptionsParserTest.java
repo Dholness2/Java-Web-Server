603d6a19c04a43750bd0b2da55ccd18a3142598c
Donovan Holness <dholness@8thlight.com>
2016-02-03 13:49:11 -0800

    updated test assert function

--- my-app/src/test/java/com/JavaWebServer/app/OptionsParserTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/OptionsParserTest.java
@@ -2,15 +2,21 @@ package com.JavaWebServer.app;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class OptionsParserTest  {
-  @Test 
+ @Test 
  public void parseArugmentsTest() {
    String [] keys = {"-p","-d"};
    String [] testArguements = {"-p","9090","-d","PUBLIC_DIR"};
-   HashMap <String,String> expectedResults = new HashMap<String, String>();
+   Map <String,String> expectedResults = new HashMap<String, String>();
+   Map <String, String> parsedArguments = OptionsParser.parse(testArguements, keys);
+
    expectedResults.put("-p", "9090");
-   expectedResults.put("-d","PUBLIC_DIR"); 
-   assertEquals(OptionsParser.parse(testArguements, keys), expectedResults);
+   expectedResults.put("-d","PUBLIC_DIR");
+   assertEquals(parsedArguments.keySet(), expectedResults.keySet());
+   System.out.println(parsedArguments.keySet());
+   System.out.println(parsedArguments.values());
  }
 }

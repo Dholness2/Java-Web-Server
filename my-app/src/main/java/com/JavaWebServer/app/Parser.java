e075e4f78a82b2831b5f074e00be87bee64c7765
Donovan Holness <dholness@8thlight.com>
2016-02-04 16:17:50 -0800

    deleted interface parser /response class

--- my-app/src/main/java/com/JavaWebServer/app/Parser.java
+++
@@ -1,9 +0,0 @@
-package com.JavaWebServer.app;
-
-import java.util.Map;
-
-public interface Parser {
-
-  public Map parse(String [] args);
-
-}
--- my-app/src/main/java/com/JavaWebServer/app/Response.java
+++
@@ -1,6 +0,0 @@
-package com.JavaWebServer.app;
-
-public class Response {
-
-
-}

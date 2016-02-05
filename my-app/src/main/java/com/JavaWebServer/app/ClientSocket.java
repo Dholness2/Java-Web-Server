5503540ec3d60af329612cde100f880d927372a7
Donovan Holness <dholness@8thlight.com>
2016-02-05 13:55:24 -0800

    removed fully qualified name

--- my-app/src/main/java/com/JavaWebServer/app/ClientSocket.java
+++ my-app/src/main/java/com/JavaWebServer/app/ClientSocket.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
+import java.net.Socket;
+
 public class  ClientSocket implements IClient {
 
-  private java.net.Socket socket;
+  private Socket socket;
 
   public ClientSocket (java.net.Socket socket) {
     this.socket = socket;

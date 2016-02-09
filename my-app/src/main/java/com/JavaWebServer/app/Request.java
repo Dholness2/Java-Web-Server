aa7244df41c893da15c2178ff15a1e1766686fc2
Donovan Holness <dholness@8thlight.com>
2016-02-08 17:25:23 -0800

    added getMethod()

--- my-app/src/main/java/com/JavaWebServer/app/Request.java
+++ my-app/src/main/java/com/JavaWebServer/app/Request.java
@@ -3,11 +3,19 @@ package com.JavaWebServer.app;
 public class Request {
   private String request;
 
-  public Request (String request) {
-    this.request = request;
+  public Request () {
+
   }
 
   public String getRoute() {
      return this.request.split(" ")[1];
+  }
+
+  public void setMessage(String message) {
+    this.request = message;
+  }
+
+  public String getMethod() {
+    return this.request.split(" ")[0];
   }
 }
--- my-app/src/test/java/com/JavaWebServer/app/RequestTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/RequestTest.java
@@ -1,16 +1,38 @@
 package com.JavaWebServer.app;
 
 import org.junit.Test;
+import org.junit.Before;
+
 import static org.junit.Assert.assertEquals;
 
 public class RequestTest {
   private Request request;
 
+  @Before
+  public void buildNewRequest() {
+    request = new Request();
+  }
+
   @Test
   public void getRoute() {
     String message = "GET / http 1.1";
     String route = "/";
-    request = new Request(message);
+    request.setMessage(message);
     assertEquals(request.getRoute(),route);
+  }
+
+
+  @Test
+  public void TestSetMessage() {
+
+
+  }
+
+  @Test
+  public void getMethod() {
+    String message = "POST / http 1.1";
+    String method ="POST";
+
+
   }
 }

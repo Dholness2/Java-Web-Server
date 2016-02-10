22cdb3a9261d11b60c822e1652ef4e45577167a5
Donovan Holness <dholness@8thlight.com>
2016-02-09 17:14:01 -0800

    added routes and method chain

--- my-app/src/main/java/com/JavaWebServer/app/App.java
+++ my-app/src/main/java/com/JavaWebServer/app/App.java
@@ -1,5 +1,6 @@
 package com.JavaWebServer.app;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
@@ -7,25 +8,39 @@ import java.util.ArrayList;
 public class App {
   private static final int PORT_INDEX = 0;
   private static final String [] KEYS = {"-p","-d"};
-
+  private static int port;
+  private static RestMethod methodChain;
 
   public static HashMap buildRoutes(){
-    ArrayList<String> routeMethods = new ArrayList<String>(); 
-    routeMethods.add("GET");
-    routeMethods.add("Put");
     HashMap<String, ArrayList<String>> routes = new HashMap<String, ArrayList<String>>();
-    routes.put("/",routeMethods);
-    routes.put("/file1",routeMethods);
-    routes .put("/text-file.txt",routeMethods);
-   return routes;
+    routes.put("/file1",routeMethods(new String [] {"GET", "POST"}));
+    routes.put("/",routeMethods(new String [] {"GET","PUT"}));
+    routes.put("/text-file.txt",routeMethods(new String [] {"GET","PUT"}));
+    routes.put("/form",routeMethods(new String [] {"POST","PUT"}));
+    routes.put("/method_options",routeMethods(new String [] {"GET","HEAD","POST","OPTIONS","PUT"}));
+    return routes;
+  }
+
+  public static RestMethod  BuildMethodChain() {
+    RestMethod putMethod = new Put();
+    RestMethod getMethod = new GetMethod();
+    RestMethod postMethod = new Post();
+    RestMethod optionsMethod = new Options();
+    getMethod.setNextMethod(putMethod);
+    putMethod.setNextMethod(optionsMethod);
+    optionsMethod.setNextMethod(postMethod);
+    return getMethod;
+  }
+
+  private static ArrayList<String> routeMethods(String [] methods) {
+   return new ArrayList<String>(Arrays.asList(methods));
   }
 
   public static void main( String[] args) throws Exception {
+    port = Integer.parseInt(OptionsParser.parse(args,KEYS).get(KEYS[PORT_INDEX]));
+    methodChain = BuildMethodChain();
 
-    Map<String,String> parsedArguments = OptionsParser.parse(args, KEYS);
-    int port = Integer.parseInt(parsedArguments.get(KEYS[PORT_INDEX]));
-
-    Responder responder = new Responder(buildRoutes());
+    Responder responder = new Responder(buildRoutes(), methodChain);
     ServerSocket serverSocket = new ServerSocket(port);
     Server app = new Server(port,serverSocket,responder);
     new Thread(app).start();

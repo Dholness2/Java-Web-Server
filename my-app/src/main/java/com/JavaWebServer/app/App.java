f3f1adc1078f91e3bf2c37e23b6df47909a3f450
Donovan Holness <dholness@8thlight.com>
2016-02-11 17:56:36 -0800

    removed method chain and added getroutes()

--- my-app/src/main/java/com/JavaWebServer/app/App.java
+++ my-app/src/main/java/com/JavaWebServer/app/App.java
@@ -9,7 +9,6 @@ public class App {
   private static final int PORT_INDEX = 0;
   private static final String [] KEYS = {"-p","-d"};
   private static int port;
-  private static RestMethod methodChain;
 
   public static HashMap buildRoutes(){
     HashMap<String, ArrayList<String>> routes = new HashMap<String, ArrayList<String>>();
@@ -18,18 +17,27 @@ public class App {
     routes.put("/text-file.txt",routeMethods(new String [] {"GET","PUT"}));
     routes.put("/form",routeMethods(new String [] {"POST","PUT"}));
     routes.put("/method_options",routeMethods(new String [] {"GET","HEAD","POST","OPTIONS","PUT"}));
+    routes.put("/redirect",routeMethods(new String [] {"GET"}));
     return routes;
   }
 
-  public static RestMethod  BuildMethodChain() {
-    RestMethod putMethod = new Put();
-    RestMethod getMethod = new GetMethod();
-    RestMethod postMethod = new Post();
-    RestMethod optionsMethod = new Options();
-    getMethod.setNextMethod(putMethod);
-    putMethod.setNextMethod(optionsMethod);
-    optionsMethod.setNextMethod(postMethod);
-    return getMethod;
+  public static HashMap getRoutes () {
+    HashMap<String, RestMethod> routes = new HashMap<String,RestMethod>();
+    routes.put("GET /file1",new Get("HTTP/1.1 200 ok"));
+    routes.put("POST /file1",new Post());
+    routes.put("GET /", new Get("HTTP/1.1 200 ok"));
+    routes.put("PUT /", new Put());
+    routes.put("GET /text-file.txt", new Get("HTTP/1.1 200 ok"));
+    routes.put("Put /text-file.txt", new Put());
+    routes.put("POST /form", new Post());
+    routes.put("PUT /form", new Put());
+    routes.put("OPTIONS /method_options", new Options());
+    routes.put("GET /method_options", new Get("HTTP/1.1 200 ok"));
+    routes.put("POST /method_options", new Post());
+    routes.put("HEAD /method_options", new Head());
+    routes.put("PUT /method_options", new Put());
+    routes.put("GET /redirect",new Get(("HTTP/1.1 302 FOUND"+"\n\r"+ "Location: http://localhost:5000/")));
+    return routes;
   }
 
   private static ArrayList<String> routeMethods(String [] methods) {
@@ -37,10 +45,10 @@ public class App {
   }
 
   public static void main( String[] args) throws Exception {
+    HashMap <String, RestMethod> routes = getRoutes();
     port = Integer.parseInt(OptionsParser.parse(args,KEYS).get(KEYS[PORT_INDEX]));
-    methodChain = BuildMethodChain();
 
-    Responder responder = new Responder(buildRoutes(), methodChain);
+    Responder responder = new Responder(buildRoutes(), routes);
     ServerSocket serverSocket = new ServerSocket(port);
     Server app = new Server(port,serverSocket,responder);
     new Thread(app).start();

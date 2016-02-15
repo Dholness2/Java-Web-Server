79edaf323bc19bda603940456ad224fae9679742
Donovan Holness <dholness@8thlight.com>
2016-02-15 13:28:47 -0800

    created status code class

---
+++ my-app/src/main/java/com/JavaWebServer/app/StatusCodes.java
@@ -0,0 +1,8 @@
+package com.JavaWebServer.app;
+
+public class StatusCodes {
+
+  public static final String OK = "HTTP/1.1 200 OK";
+  public static final String NOT_FOUND = "HTTP/1.1 404 Not Found";
+  public static final String FOUND = "HTTP/1.1 302 FOUND";
+}

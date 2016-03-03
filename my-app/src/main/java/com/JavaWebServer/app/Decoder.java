8b4333c15cdd9abb0a45c73e303f6a43a83dc3ba
Donovan Holness <dholness@8thlight.com>
2016-03-02 16:22:31 -0800

    created params decoder

---
+++ my-app/src/main/java/com/JavaWebServer/app/Decoder.java
@@ -0,0 +1,19 @@
+package com.JavaWebServer.app;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Decoder {
+   private static String response;
+
+   public static String  decode(Map<String, String> encoding, String code) {
+     response = code;
+     encoding.forEach((k,v)->{
+       if (code.contains(k)){
+         response = response.replaceAll(k,v);
+       }
+     });
+     return response;
+   }
+}

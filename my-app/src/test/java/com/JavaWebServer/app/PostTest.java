974aae4e814cdbdac5a599a5c318fefa28189717
Donovan Holness <dholness@8thlight.com>
2016-03-03 17:01:49 -0800

    updated handleRequest in postTest

--- my-app/src/test/java/com/JavaWebServer/app/PostTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/PostTest.java
@@ -7,9 +7,10 @@ public class PostTest {
 
   @Test
   public void handleRequestTest() {
+    Request testRequest = new Request();
     StatusCodes codes = new StatusCodes();
     RestMethod testPost = new Post(codes.OK);
-    String response = new String (testPost.handleRequest());
+    String response = new String (testPost.handleRequest(testRequest));
     assertEquals(codes.OK, response);
   }
 }

cceee3f7f8510bdbab4c53ac9f3f68e91f1a4349
Donovan Holness <dholness@8thlight.com>
2016-03-03 17:12:40 -0800

    added params Request test

--- my-app/src/test/java/com/JavaWebServer/app/RequestTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/RequestTest.java
@@ -9,9 +9,23 @@ import static org.junit.Assert.assertEquals;
 public class RequestTest {
   private Request request;
 
+  private String message = "GET /parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff HTTP/1.1";
+
   @Before
   public void buildNewRequest() {
     request = new Request();
+  }
+
+  @Test public void getParamTest() {
+   request.setMessage(message);
+   String expected = "variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff";
+   assertEquals(request.getParams(),expected);
+  }
+
+  @Test public void getParamsRouteTest() {
+    request.setMessage(message);
+    String expected =  "/parameters?";
+    assertEquals(expected,request.getParamsRoute());
   }
 
   @Test

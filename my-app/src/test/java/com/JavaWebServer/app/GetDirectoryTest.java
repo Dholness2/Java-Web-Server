62798d418446fd88cb25bcb86fb78847dc0627e7
dholness2 <DHolness@Dons-MacBook-Pro.local>
2016-03-05 20:01:22 -0800

    updated expected results

--- my-app/src/test/java/com/JavaWebServer/app/GetDirectoryTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/GetDirectoryTest.java
@@ -12,9 +12,9 @@ public class GetDirectoryTest {
 
   private Map <String, String> directoryLinks = new LinkedHashMap <String, String> ();
   private StatusCodes codes = new StatusCodes();
-  private final String CRLF = "\r\n";
-  private final String URL = "http://localhost:5000";
-  private final String CONTENTYPE = "text/html";
+  private  final String CRLF = "\r\n";
+  private  final String CONTENTYPE = "text/html";
+  private  String directory = "/Users/DHolness/desktop/cob_spec/public/";
 
   private String buildHeader (String body, String type) {
     String typeHeader = "Content-Type: ";
@@ -25,26 +25,18 @@ public class GetDirectoryTest {
         + length);
   }
 
-  @Before
-  public void buildLinks() {
-    directoryLinks.put("/file1","file1");
-    directoryLinks.put("/file2","file2");
-    directoryLinks.put("/image.gif","image.gif");
-    directoryLinks.put("/image.jpeg","image.jpeg");
-    directoryLinks.put("/image.png","image.png");
-    directoryLinks.put("/text-file.txt","text-file.txt");
-  }
-
   @Test
   public void handleResponseTest(){
-    RestMethod testGetDir = new GetDirectory(codes,directoryLinks,URL);
+    RestMethod testGetDir = new GetDirectory(codes,directory);
     byte [] response = testGetDir.handleRequest(new Request());
-    String body = "<!DOCTYPE html><html><body><a href=\""+URL+"/file1\">file1</a><br>"+CRLF+
-                   "<a href=\""+URL+"/file2\">file2</a><br>"+CRLF+
-                  "<a href=\""+URL+"/image.gif\">image.gif</a><br>"+CRLF+
-                   "<a href=\""+URL+"/image.jpeg\">image.jpeg</a><br>"+CRLF+
-                   "<a href=\""+URL+"/image.png\">image.png</a><br>"+CRLF+
-                   "<a href=\""+URL+"/text-file.txt\">text-file.txt</a><br></body></html>";
+    String body = "<!DOCTYPE html><html><body><a href=\"/file1\">file1</a><br>"+CRLF+
+                   "<a href=\"/file2\">file2</a><br>"+CRLF+
+                  "<a href=\"/image.gif\">image.gif</a><br>"+CRLF+
+                   "<a href=\"/image.jpeg\">image.jpeg</a><br>"+CRLF+
+                   "<a href=\"/image.png\">image.png</a><br>"+CRLF+
+	           "<a href=\"/partial_content.txt\">partial_content.txt</a><br>"+CRLF+
+                   "<a href=\"/patch-content.txt\">patch-content.txt</a><br>"+CRLF+
+		   "<a href=\"/text-file.txt\">text-file.txt</a><br></body></html>";
     String expected = buildHeader(body, CONTENTYPE)+CRLF+CRLF+body;
     assertEquals(expected, new String(response));
   }

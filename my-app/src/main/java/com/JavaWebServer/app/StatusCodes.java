1b164ee607cfc3168b6cdaa707c328021e445adc
Donovan Holness <dholness@8thlight.com>
2016-03-15 19:12:20 -0700

    add 206 status code

--- my-app/src/main/java/com/JavaWebServer/app/StatusCodes.java
+++ my-app/src/main/java/com/JavaWebServer/app/StatusCodes.java
@@ -4,4 +4,7 @@ public class StatusCodes {
   public static final String OK = "HTTP/1.1 200 OK";
   public static final String NOT_FOUND = "HTTP/1.1 404 Not Found";
   public static final String FOUND = "HTTP/1.1 302 FOUND";
+  public static final String PARTIAL = "HTTP/1.1 206 Partial Content";
+
+
 }

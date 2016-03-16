b0b6ec20456e467d817b5027da4b9f6b4b9070be
Donovan Holness <dholness@8thlight.com>
2016-03-15 20:09:24 -0700

    added error codes

--- my-app/src/main/java/com/JavaWebServer/app/StatusCodes.java
+++ my-app/src/main/java/com/JavaWebServer/app/StatusCodes.java
@@ -5,6 +5,6 @@ public class StatusCodes {
   public static final String NOT_FOUND = "HTTP/1.1 404 Not Found";
   public static final String FOUND = "HTTP/1.1 302 FOUND";
   public static final String PARTIAL = "HTTP/1.1 206 Partial Content";
-
-
+  public static final String RANGE_NOT_SATSIFIABLE = "HTTP/1.1 416 Range Not Satisfiable";
+  public static final String STATUSERROR = "HTTP/1.1 500 Internal Server Error";
 }

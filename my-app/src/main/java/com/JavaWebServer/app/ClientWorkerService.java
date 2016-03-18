51ce835108e5ddc98c6e8978fc17c88af13ad2c7
Donovan Holness <dholness@8thlight.com>
2016-03-18 14:56:18 -0700

    added logger to Client workers

--- my-app/src/main/java/com/JavaWebServer/app/ClientWorkerService.java
+++ my-app/src/main/java/com/JavaWebServer/app/ClientWorkerService.java
@@ -5,16 +5,29 @@ import java.util.ArrayList;
 public class ClientWorkerService implements Runnable {
   private final ClientSocket client;
   private final Responder responder;
+  private final Logger logger;
+  private Request request;
 
-  public ClientWorkerService (ClientSocket client, Responder responder) {
+  public ClientWorkerService (ClientSocket client, Responder responder, Logger logger) {
     this.client = client;
     this.responder = responder;
+    this.logger = logger;
   }
 
-  public  void run () {
-    Request request = client.getRequest();
-    client.sendResponse(this.responder.getResponse(request));
+  public void run () {
+    this.request = client.getRequest();
+    log();
+    client.sendResponse(getResponse());
     client.close();
     System.out.println ("Server: request Closed");
+  }
+
+
+  private byte [] getResponse() {
+    return this.responder.getResponse(this.request);
+  }
+
+  private void log() {
+    this.logger.logRequest(this.request);
   }
 }

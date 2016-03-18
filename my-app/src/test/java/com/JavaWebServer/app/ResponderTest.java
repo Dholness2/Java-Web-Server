336b93ba52077f03da5a0e155c84ecb32d9fdc11
Donovan Holness <dholness@8thlight.com>
2016-03-18 10:59:05 -0700

    Updated responder test
     *Removed encoding key
     *Updated Params args

--- my-app/src/test/java/com/JavaWebServer/app/ResponderTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/ResponderTest.java
@@ -17,35 +17,12 @@ public class ResponderTest {
   private StatusCodes codes = new StatusCodes();
   private String request = "GET /paramaters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff HTTP/1.1";
 
-  private  Map <String, String>  paramatersEncodingKeyMap() {
-    Map<String, String> encode = new HashMap<String, String>();
-    encode.put("%3C","<");
-    encode.put("%3E",">");
-    encode.put("%3D","=");
-    encode.put("%2C",",");
-    encode.put("%21","!" );
-    encode.put("%2B","+");
-    encode.put("%2D","-");
-    encode.put("%20"," ");
-    encode.put("%22", "\"");
-    encode.put("%3B",";");
-    encode.put("%2A","*");
-    encode.put("%26","&");
-    encode.put("%40","@");
-    encode.put("%23","#");
-    encode.put("%24","\\$");
-    encode.put("%5B","[");
-    encode.put("%5D","]");
-    encode.put("%3A",":");
-    encode.put("%3F","?");
-    return encode;
-  }
 
   @Before
   public void buildTestSetup()  {
      HashMap<String, RestMethod> methods = new HashMap<String,RestMethod>();
      methods.put("GET /", new Get(codes.OK));
-     methods.put("GET /paramaters?", new Params(codes.OK, "paramaters?", paramatersEncodingKeyMap()));
+     methods.put("GET /paramaters?", new Params(codes.OK, "paramaters?"));
      ArrayList<String> routeMethods = new ArrayList<String>(); 
      routeMethods.add("GET");
      routes = new HashMap<String, ArrayList<String>>();

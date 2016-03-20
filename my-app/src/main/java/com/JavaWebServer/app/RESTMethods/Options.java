6488ff26295128a9fe75fab0da1238d058971b65
dholness2 <DHolness@Dons-MacBook-Pro.local>
2016-03-20 16:59:58 -0700

    Refactor of Option.java

--- my-app/src/main/java/com/JavaWebServer/app/RESTMethods/Options.java
+++ my-app/src/main/java/com/JavaWebServer/app/RESTMethods/Options.java
@@ -1,16 +1,36 @@
 package com.JavaWebServer.app;
 
-public class Options implements RestMethod {
-  private String responseStatus;
+import java.util.HashMap;
+import java.util.ArrayList;
 
-  public Options(String response) {
-    this.responseStatus = response;
+public class Options implements RestMethod {
+  private String status;
+  private String allowHeader= "Allow: ";
+  private HashMap<String,ArrayList<String>> directory;
+  private String CRLF = System.getProperty("line.separator");
+  
+  
+  public Options(String response,HashMap<String,ArrayList<String>> directory ) {
+    this.status = response;
+    this. directory = directory;
   }
 
   public byte [] handleRequest(Request request) {
-    StringBuilder response = new StringBuilder();
-    response.append(this.responseStatus + System.lineSeparator());
-    response.append("Allow: GET,HEAD,POST,OPTIONS,PUT");
-    return response.toString().getBytes();
+    ArrayList methods = getAllowedMethods(request);
+    return (status+ CRLF + buildAllowedHeader(methods)).getBytes();
   }
+
+  private ArrayList<String> getAllowedMethods(Request request) {
+    String route = request.getRoute();
+    return directory.get(route);
+ }
+
+  private String buildAllowedHeader(ArrayList<String> methods) {
+    StringBuilder allowed = new StringBuilder();
+    allowed.append(this.allowHeader);
+    for (String  restMethod : methods) {   
+      allowed.append(restMethod+",");
+    }
+    return allowed.toString();
+ }
 }

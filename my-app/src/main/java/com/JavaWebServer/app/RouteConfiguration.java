ace0cdcc23994801006855baf664649b43c8e00a
Donovan Holness <dholness@8thlight.com>
2016-03-29 10:31:40 -0700

    added RouteConfiguration class

---
+++ my-app/src/main/java/com/JavaWebServer/app/RouteConfiguration.java
@@ -0,0 +1,5 @@
+package com.JavaWebServer.app;
+
+public class RouteConfiguration {
+  public RouteConfiguration () {}
+}
---
+++ my-app/src/test/java/com/JavaWebServer/app/RouteConfigurationTest.java
@@ -0,0 +1,20 @@
+package com.JavaWebServer.app;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class RouteConfigurationTest {
+
+  @Test
+  public void buildDirectoryRoutesTest() {
+   RouteConfiguration routeConfig = new RouteConfiguration();
+  }
+}

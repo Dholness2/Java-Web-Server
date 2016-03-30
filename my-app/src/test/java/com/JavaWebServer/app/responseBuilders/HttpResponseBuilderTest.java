3b1195d993afeac27b46afc9c3092fd2827da9a1
Donovan Holness <dholness@8thlight.com>
2016-03-30 09:16:30 -0700

    Fixed ResponseBuilder ClearMethod

--- my-app/src/test/java/com/JavaWebServer/app/responseBuilders/HttpResponseBuilderTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/responseBuilders/HttpResponseBuilderTest.java
@@ -6,13 +6,12 @@ import org.junit.After;
 import static org.junit.Assert.assertEquals;
 
 public class HttpResponseBuilderTest {
-  private ResponseBuilder testResponseBuilder = new HttpResponseBuilder(); 
+  private HttpResponseBuilder testResponseBuilder = new HttpResponseBuilder();
   private String CRLF = System.getProperty("line.separator");
-
 
   @After
   public void resetResponse(){
-    testResponseBuilder = new HttpResponseBuilder();
+    testResponseBuilder.clearBuilder();
   }
 
   @Test
@@ -47,6 +46,22 @@ public class HttpResponseBuilderTest {
     testResponseBuilder.addBody(body);
     String response = (new String (testResponseBuilder.getResponse()));
     String expectedResponse = "HTTP/1.1 200 OK"+CRLF+"Content-Length: 11"+CRLF+CRLF+CRLF+"Hello World";
+    assertEquals(expectedResponse, response);
+  }
+
+  @Test
+  public void clearBuilderTest() {
+    String OK = "OK";
+    byte[] body = ("First Response").getBytes();
+    String header ="Content-Length: ";
+    String value = Integer.toString(body.length);
+    testResponseBuilder.addStatus(OK);
+    testResponseBuilder.addHeader(header,value);
+    testResponseBuilder.addBody(body);
+    testResponseBuilder.clearBuilder();
+    testResponseBuilder.addStatus("NOT_FOUND");
+    String response = (new String (testResponseBuilder.getResponse()));
+    String expectedResponse = "HTTP/1.1 404 Not Found"+ CRLF;
     assertEquals(expectedResponse, response);
   }
 }

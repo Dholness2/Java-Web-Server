ab3230620268c7a6a2e93a249409f7ce5c04e3ae
Donovan Holness <dholness@8thlight.com>
2016-03-30 18:38:53 -0700

    Added ResponseBuilder to GetFormTest

--- my-app/src/test/java/com/JavaWebServer/app/responses/GetFormTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/responses/GetFormTest.java
@@ -7,40 +7,72 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import com.JavaWebServer.app.responses.Response;
+import com.JavaWebServer.app.responseBuilders.ResponseBuilder;
+import com.JavaWebServer.app.responseBuilders.HttpResponseBuilder;
 import com.JavaWebServer.app.responses.GetForm;
 import com.JavaWebServer.app.responses.Get;
-import com.JavaWebServer.app.StatusCodes;
 import com.JavaWebServer.app.Request;
 
-public class GetFormTest {
-  private String CRLF = System.getProperty("line.separator");
-  private String directory = System.getProperty("user.dir");
-  private String formData = "My=Data";
-  private StatusCodes testCodes = new StatusCodes();
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.io.File;
 
+public class GetFormTest {
+  private ResponseBuilder response = new HttpResponseBuilder();
+  private String formData = "My=Data";
+  private String fileName ="/form";
+  private  String fileType = "text/plain";
+  private String directory = System.getProperty("user.dir");
+  private boolean authenticationEnabled = false;
+  private GetForm testGetForm = new GetForm(response,fileName,fileType,directory,authenticationEnabled,formData);
+
+  private String buildResponse (String path, String contentType) throws IOException{
+    String typeHeader = "Content-Type: ";
+    String contentLength = "Content-Length: ";
+    String fileLength = String.valueOf(getFile(path).length);
+    this.response.addStatus("OK");
+    this.response.addHeader(typeHeader, contentType);
+    this.response.addHeader(contentLength,(fileLength));
+    this.response.addBody(formData.getBytes());
+    String response = new String (this.response.getResponse());
+    this.response.clearBuilder();
+    return response;
+  }
+
+  private byte [] getFile(String path) throws IOException {
+    Path location = Paths.get(path);
+    return Files.readAllBytes(location);
+  }
 
   @Test
-  public void handleRequestNewFormFileTest() {
-    Get testgetform = new GetForm(testCodes,"form","text/plain",directory,false,formData);
-    byte [] response = testgetform.handleRequest(new Request());
+  public void hasFileNonExistantFileTest() {
+    testGetForm.buildPath();
+    assertFalse(this.testGetForm.hasFile());
+  }
+
+  @Test
+  public void hasFilePresentFileTest() {
+    Request request = new Request();
+    this.testGetForm.handleRequest(request);
+    assertTrue(testGetForm.hasFile());
+  }
+
+  @Test
+  public void buildFormFileTest() {
+    GetForm testGetForm = new GetForm(response,fileName,fileType,directory,authenticationEnabled,formData);
+    testGetForm.buildPath();
+    testGetForm.buildFormFile();
+    assertTrue(testGetForm.hasFile());
+  }
+
+  @Test
+  public void handleRequestNewFormFileTest() throws Exception {
+    Get testGetForm = new GetForm(response,fileName,fileType,directory,authenticationEnabled,formData);
+    byte [] response = testGetForm.handleRequest(new Request());
     String testResponse = new String(response);
-    String expectedResponse = "";
+    String expectedResponse = buildResponse((directory+fileName),fileType);
     assertEquals(expectedResponse,testResponse);
   }
-
-  @Test
-  public void fileExistTest() {
-    GetForm testGetForm = new GetForm(testCodes,"/form","text/plain",directory,true,formData);
-    assertFalse(testGetForm.hasFile());
-  }
-
-  @Test
-  public void getNewFormResponse() {
-    int bodyIndex = 1;
-    int header = 0;
-    GetForm testGetForm = new GetForm(testCodes,"/form","text/plain",directory,true,formData);
-    String [] response = new String(testGetForm.getNewFormResponse()).split(CRLF+CRLF);
-    String bodyResponse = response[bodyIndex];
-    assertEquals(formData,response);
-  }
- }
+}

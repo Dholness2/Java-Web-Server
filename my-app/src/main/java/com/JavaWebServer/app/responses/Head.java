e9c4a0c263cfe0328aa7af38958f9c0a3328385a
Donovan Holness <dholness@8thlight.com>
2016-03-31 11:44:34 -0700

    extended Head class from Get.Java

--- my-app/src/main/java/com/JavaWebServer/app/responses/Head.java
+++ my-app/src/main/java/com/JavaWebServer/app/responses/Head.java
@@ -1,16 +1,12 @@
 package com.JavaWebServer.app.responses;
 
 import com.JavaWebServer.app.responses.Response;
+import com.JavaWebServer.app.responses.Get;
 import com.JavaWebServer.app.Request;
 
-public class Head implements Response {
-  private String responseStatus;
+public class Head extends Get {
 
-  public Head(String response ) {
-    this.responseStatus = response;
-  }
-
-  public byte [] handleRequest(Request request) {
-      return this.responseStatus.getBytes();
+  public Head(String response) {
+    super(response);
   }
 }

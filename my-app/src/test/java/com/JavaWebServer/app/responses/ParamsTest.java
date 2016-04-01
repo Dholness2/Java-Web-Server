c0f3ce51353d436b512cd43f27c36de05e98146e
Donovan Holness <dholness@8thlight.com>
2016-04-01 15:41:00 -0700

    updated ParamsTest

--- my-app/src/test/java/com/JavaWebServer/app/responses/ParamsTest.java
+++ my-app/src/test/java/com/JavaWebServer/app/responses/ParamsTest.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import com.JavaWebServer.app.responses.Response;
 import com.JavaWebServer.app.responses.Params;
-import com.JavaWebServer.app.StatusCodes;
+import com.JavaWebServer.app.responseBuilders.HttpResponseBuilder;
 import com.JavaWebServer.app.Request;
 
 import org.junit.Test;
@@ -14,29 +14,31 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ParamsTest {
-  private StatusCodes codes = new StatusCodes();
-  private String CRLF = "\r\n";
-  private String request = "GET /parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff HTTP/1.1";
-
-
+  private String CRLF = System.getProperty("line.separator");;
+  private String request = "GET /parameters?variable_1=Operators%20%3C"+
+                           "%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20"+
+                           "-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24"+
+                           "%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22"+
+                           "%3F&variable_2=stuff HTTP/1.1";
   private Request testRequest;
   private Params testParams;
 
   @Before
-  public void buildObjects () {
+  public void buildRequiredObjects() {
     testRequest = new Request();
     testRequest.setMessage (request);
     String params = testRequest.getParams();
-    testParams = new Params(codes.OK,"paramaters?");
+    testParams = new Params(new HttpResponseBuilder(), "paramaters?");
   }
 
   @Test
   public void handleRequest() {
-    String content = "Content-Length:97";
+    String status = "HTTP/1.1 200 OK";
+    String content = "Content-Length: 96";
     String type = "Content-Type: text/plain";
     String variableOne = "variable_1 = Operators <, >, =, !=; +, -, *, &, @, #, $, [, ]: \"is that all\"?";
     String variableTwo = "variable_2 = stuff";
-    String expectedResponse = codes.OK +CRLF+type+CRLF+content+CRLF+CRLF+variableOne+CRLF+variableTwo;
+    String expectedResponse = status + CRLF + type + CRLF + content + CRLF + CRLF+ variableOne + CRLF + variableTwo;
     assertEquals(expectedResponse, new String(testParams.handleRequest(testRequest)));
   }
 }

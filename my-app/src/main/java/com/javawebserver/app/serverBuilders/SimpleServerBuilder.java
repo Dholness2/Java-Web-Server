e9bb837c154fb08e91d8670fc0f2e7d631539caf
Donovan Holness <dholness@8thlight.com>
2016-04-07 15:47:47 -0700

    added ServerBuilder class

---
+++ my-app/src/main/java/com/javawebserver/app/serverBuilders/SimpleServerBuilder.java
@@ -0,0 +1,36 @@
+package com.javawebserver.app.serverBuilders;
+
+import com.javawebserver.app.Server;
+import com.javawebserver.app.Responder;
+import com.javawebserver.app.helpers.Logger;
+import com.javawebserver.app.RouteBuilder;
+import com.javawebserver.app.serverSockets.ServerSocket;
+import com.javawebserver.app.serverSockets.ServerSocketWrapper;
+import com.javawebserver.app.responses.Response;
+
+import java.io.IOException;
+
+import java.util.HashMap;
+
+public class SimpleServerBuilder {
+  private String directory = System.getProperty("user.dir") + "/logs";
+  private RouteBuilder routes = new RouteBuilder(this.directory);
+
+  public SimpleServerBuilder() {}
+
+  public Server getServer(int port) throws IOException  {
+    Responder responder = new Responder(routes.getRoutes());
+    ServerSocket serverSocket = new ServerSocketWrapper(port);
+    Logger logger = new Logger(this.directory);
+    Server server = new Server(port, serverSocket,responder,logger);
+    return server;
+  }
+
+  public void addRoute(String route, Response response) {
+    this.routes.addRoute(route,response);
+  }
+
+  public HashMap<String, Response>  getRoutes() {
+    return this.routes.getRoutes();
+  }
+}

3b06a366ef6b4818274a8801c9603c3564c98bdf
Donovan Holness <dholness@8thlight.com>
2016-04-12 15:01:28 -0700

    modified ResponseBuilder interface

--- my-app/src/main/java/com/javawebserver/app/responseBuilders/ResponseBuilder.java
+++ my-app/src/main/java/com/javawebserver/app/responseBuilders/ResponseBuilder.java
@@ -1,9 +1,11 @@
 package com.javawebserver.app.responseBuilders;
 
 public interface ResponseBuilder {
-  public byte [] getResponse();
-  public void addHeader(String header,String headerValue);
-  public void addBody (byte [] body);
+  public ResponseBuilder clone();
+  public byte[] getStatus(String status);
+  public byte[] getResponse();
+  public void addHeader(String header, String headerValue);
+  public void addBody(byte[] body);
   public void addStatus(String status);
   public void clearBuilder();
 }

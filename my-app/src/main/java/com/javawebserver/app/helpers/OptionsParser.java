02de8b461e7b5c4829d67da1ab62c2090ecda5fe
Donovan Holness <dholness@8thlight.com>
2016-04-12 21:36:22 -0700

    refactored OptionsParser class

--- my-app/src/main/java/com/javawebserver/app/helpers/OptionsParser.java
+++ my-app/src/main/java/com/javawebserver/app/helpers/OptionsParser.java
@@ -2,20 +2,70 @@ package com.javawebserver.app;
 
 import java.util.Map;
 import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.io.File;
 
 public class OptionsParser {
+  private Scanner input;
+  private String[] args;
 
-  public static Map<String,String> parse (String [] args, String [] keys) {
+  private static final int PORT_INDEX = 0;
+  private static final int DIR_INDEX = 1;
+  private static final String[] KEYS = {"-p","-d"};
+
+  public OptionsParser (Scanner input, String[] args) {
+    this.input = input;
+    this.args = args;
+  }
+
+  public String getDirectory() {
+    String directory;
+    directory = parseArgs().get(KEYS[DIR_INDEX]);
+    File dir = new File(directory);
+      if (dir.exists()) {
+        return  dir.getAbsolutePath();
+      }
+    return getCorrectFilePath();
+  }
+
+  private String getCorrectFilePath() {
+    System.out.print("Path does not exist, enter correct path:");
+    String path = input.nextLine();
+    File dir = new File(path);
+      if (dir.exists()) {
+        return dir.getAbsolutePath();
+      }
+    return getCorrectFilePath();
+  }
+
+  public int getPort(){
+    int port;
+    try {
+      port = Integer.parseInt(parseArgs().get(KEYS[PORT_INDEX]));
+    } catch (NumberFormatException e) {
+      port = getNewPort();
+
+    }
+    return port;
+  }
+
+  private Map<String,String> parseArgs() {
     Map<String,String> results = new LinkedHashMap();
-    int argsSize = args.length;
-    int keysSize = keys.length;
-    for(int x = 0; x < keysSize; x++) {
-      for(int y = 0; y < argsSize; y++) {
-        if (keys[x].equals(args[y])) {
-          results.put(keys[x],args[(y + 1)]);
+    for(int x = 0; x < KEYS.length; x++) {
+      for(int y = 0; y < this.args.length; y++) {
+        if (KEYS[x].equals(args[y])) {
+          results.put(KEYS[x],args[(y + 1)]);
         }
       }
     }
     return results;
+  }
+
+  private int getNewPort () {
+    System.out.print("Invalid port number please enter new port:");
+    if (input.hasNextInt()){
+      return input.nextInt();
+    }
+    return getNewPort();
   }
 }
--- my-app/src/test/java/com/javawebserver/app/helpers/OptionsParserTest.java
+++ my-app/src/test/java/com/javawebserver/app/helpers/OptionsParserTest.java
@@ -1,20 +1,92 @@
 package com.javawebserver.app;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
 public class OptionsParserTest  {
+  private final ByteArrayOutputStream outPut = new ByteArrayOutputStream();
 
- @Test
- public void parseArugmentsTest() {
-   String [] keys = {"-p","-d"};
-   String [] testArguements = {"-p","9090","-d","PUBLIC_DIR"};
-   Map <String,String> expectedResults = new HashMap<String, String>();
-   Map <String, String> parsedArguments = OptionsParser.parse(testArguements, keys);
-   expectedResults.put("-p", "9090");
-   expectedResults.put("-d","PUBLIC_DIR");
-   assertEquals(parsedArguments.keySet(), expectedResults.keySet());
- }
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outPut));
+  }
+
+  @After
+  public void cleanUpStreams() {
+    outPut.reset();
+  }
+
+  @Test
+  public void invalidPortNumberTest() {
+    String[] testArgs = {"-p","500I"};
+    String inputData = "5000";
+    System.setIn(new java.io.ByteArrayInputStream(inputData.getBytes()));
+    Scanner testScanner =  new Scanner(System.in);
+    OptionsParser testParser = new OptionsParser(testScanner, testArgs);
+    int port = testParser.getPort();
+    assertEquals(5000, port);
+  }
+
+  @Test
+  public void respondsToInvalidPortTest() {
+    String[] testArgs = {"-p","500I"};
+    String inputData = "5000";
+    System.setIn(new java.io.ByteArrayInputStream(inputData.getBytes()));
+    Scanner testScanner =  new Scanner(System.in);
+    OptionsParser testParser = new OptionsParser(testScanner, testArgs);
+    testParser.getPort();
+    String expectedOutPut = "Invalid port number please enter new port:";
+    assertEquals(expectedOutPut, outPut.toString());
+  }
+
+  @Test
+  public void getsValidPortNumberTest() {
+    String[] testArgs = {"-p","5000"};
+    Scanner testScanner =  new Scanner(System.in);
+    OptionsParser testParser = new OptionsParser(testScanner, testArgs);
+    int port = testParser.getPort();
+    assertEquals(5000, port);
+  }
+
+  @Test
+  public void checksInvalidDirectoriesTest() {
+    String[] testArgs = {"-d","helloWorld"};
+    String inputData = "/";
+    System.setIn(new java.io.ByteArrayInputStream(inputData.getBytes()));
+    Scanner testScanner =  new Scanner(System.in);
+    OptionsParser testParser = new OptionsParser(testScanner, testArgs);
+    String directory = testParser.getDirectory();
+    assertEquals("/", directory);
+  }
+
+  @Test
+  public void respondsToInvalidDirectoryTest() {
+    String[] testArgs = {"-d","helloWorld"};
+    String inputData = "/";
+    System.setIn(new java.io.ByteArrayInputStream(inputData.getBytes()));
+    Scanner testScanner = new Scanner(System.in);
+    OptionsParser testParser = new OptionsParser(testScanner, testArgs);
+    testParser.getDirectory();
+    String expectedOutPut = "Path does not exist, enter correct path:";
+    assertEquals(expectedOutPut, outPut.toString());
+  }
+
+  @Test
+  public void getsValidDirectoriesTest() {
+    String[] testArgs = {"-d", "/"};
+    Scanner testScanner =  new Scanner(System.in);
+    OptionsParser testParser = new OptionsParser(testScanner, testArgs);
+    String directory = testParser.getDirectory();
+    assertEquals("/", directory);
+  }
 }
